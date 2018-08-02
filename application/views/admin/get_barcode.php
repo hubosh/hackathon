93bef9729ca7f51f997ad04cb6beeff3d7baf6e4
zchacker <ahmedadmnagem@gmail.com>
2018-08-03 00:25:17 +0300

    project files

---
+++ application/views/admin/get_barcode.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>تسجيل مستخدم</title>
+  </head>
+  <body>
+    <form class="" action="" method="post">
+      <table>
+        <tbody>
+
+          <tr>
+            <td> <label for="">رقم الباركود</label> </td>
+            <td> <input type="text" name="barcode" value=""  required /> </td>
+          </tr>
+
+          <tr>
+            <td>  </td>
+            <td> <input type="submit" name="" value="حفظ"> </td>
+          </tr>
+
+        </tbody>
+      </table>
+    </form>
+  </body>
+</html>
---
+++ application/views/medical/question_page.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>سؤال</title>
+  </head>
+  <body>
+    <h1>هل انت مسجل بالنظام؟</h1>
+
+    <div class="left">
+      
+    </div>
+
+    <div class="right">
+
+    </div>
+
+  </body>
+</html>
