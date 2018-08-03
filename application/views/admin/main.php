effcf9de689e0c5b2aec1467c243736dff4d3d9e
Heba Alkhalifa <hubosh1994@gmail.com>
2018-08-03 05:55:06 +0300

    final

--- application/views/admin/main.php
+++ application/views/admin/main.php
@@ -3,63 +3,34 @@
   <head>
     <meta charset="utf-8">
     <title>الشاشة الرئيسية</title>
-    <style media="screen">
-      .div1{
-        background-color: brown;
-        width: 400px;
-        display: block;
-        height: 200px;
-        color: #FFFFFF;
-        margin-left: auto;
-        margin-right: auto;
-        margin-bottom: 50px;
-        padding-left: auto;
-        padding-right: auto;
-      }
-      .div1 a{
-        color: #FFFFFF;
-        text-decoration: none;
-      }
-    </style>
   </head>
   <body>
 
-    <div class="div1">
-      <center>
-        <a href=""><h2>أين أنا</h2></a>
-      </center>
-    </div>
+    <div class="container">
+        <div class="services">
+            <div class="row">
+                <div class="col-md-4 col-sm-6 service text-center">
+                    <img src="img/service-1.png">
+                </div>
+                <div class="col-md-4 col-sm-6 service text-center">
+                    <img src="img/service-2.png">
+                </div>
+                <div class="col-md-4 col-sm-6 service text-center">
+                    <img src="img/service-3.png">
+                </div>
 
-    <div class="div1">
-      <center>
-        <a href="<?=base_url();?>/index.php/admin/lost_user"><h2>أنا تائه</h2></a>
-      </center>
+                <div class="col-md-4 col-sm-6 service text-center">
+                    <img src="img/service-4.png">
+                </div>
+                <div class="col-md-4 col-sm-6 service text-center">
+                    <img src="img/service-5.png">
+                </div>
+                <div class="col-md-4 col-sm-6 service text-center">
+                    <img src="img/service-6.png">
+                </div>
+            </div>
+        </div>
+        
     </div>
-
-    <div class="div1">
-      <center>
-        <a href="<?=base_url()?>index.php/admin/register_users"><h2> سجل مجموعتك حتى لا تضيع!</h2></a>
-      </center>
-    </div>
-
-    <div class="div1">
-      <center>
-        <a href="<?=base_url()?>index.php/admin/edit_users"><h2> تفاصيل مجموعتك </h2></a>
-      </center>
-    </div>
-
-    <div class="div1">
-      <center>
-        <a href=""><h2>أين أجد مطاعم</h2></a>
-      </center>
-    </div>
-
-    <div class="div1">
-      <center>
-        <a href=""><h2>أحتاج مساعدة طبية</h2></a>
-      </center>
-    </div>
-    
-
   </body>
 </html>
--- application/views/inc/header.php
+++ application/views/inc/header.php
@@ -20,32 +20,26 @@
   </head>
 
 <body>
-    <div class="navbar-wrapper hidden-lg hidden-md hidden-sm">
+<body>
+    <div class="navbar-wrapper">
         <nav class="navbar navbar-static-top navbar-expand-lg navbar-dark navbar-inverse">
             <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false"
-                        aria-controls="navbar">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="#">Maakom</a>
+                <div class="navbar-header pull-left">
+                    <a class="navbar-brand" href="#"><img class="logo" src="img/logo.png"></a>
                 </div>
-                <div id="navbar" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav navbar-right">
                     <ul class="nav navbar-nav">
-                        <li class="active">
-                            <a href="#">Home</a>
-                        </li>
-                        <li>
-                            <a href="#about">About</a>
-                        </li>
-                        <li>
-                            <a href="#contact">Contact</a>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" href="#">English
+                            <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                            <li><a href="#">EN</a></li>
+                            <li><a href="#">AR</a></li>
+                            <li><a href="#">TR</a></li>
+                            </ul>
                         </li>
                     </ul>
-                </div>
+                </ul>
             </div>
         </nav>
     </div>
